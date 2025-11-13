c8cd3ff3b933360adfa7252e3d3093473cd6cea8
Ariel Eliahu <ariel.eliahu@post.runi.ac.il>
2025-11-13 21:56:41 +0200

    Completed assignment

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,38 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        
+        // inputs
+	    String word = args[0];
+        int printTimes = Integer.parseInt(args[1]);
+
+        // string to uppercase
+        word = word.toUpperCase();
+
+        // intigers used later for while
+        int i = 0;
+        int printWord = 0;
+        int length = word.length();
+
+        // special characters string
+        String special_c = "AEFHILMNORSX";
+
+        // print by characters
+        while (printTimes > 0 && i < length) {
+            char c = word.charAt(i);
+            if (special_c.indexOf(c) != -1) {
+                System.out.println("Give me an: " + c + "!");
+            }
+            else {
+               System.out.println("Give me a: " + c + "!"); 
+            }
+            i += 1;
+        }
+        System.out.println("What does that spell?");
+
+        // count the number of times that the word should be printed
+        while (printWord < printTimes) {
+                System.out.println(word + "!!!");
+                printWord += 1;
+            }
         }
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,31 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+        String minutes_updated;
+        String hours_updated;
+
+        int total_minutes = (hours * 60) + minutes + minutesToAdd;
+        int total_hours = total_minutes / 60;
+        int hours1 = total_hours % 24;
+        int minutes1 = total_minutes - (total_hours * 60);
+
+        if (minutes1 >= 0 && minutes1 < 10) {
+		minutes_updated = "0" + minutes1; }
+else {
+    minutes_updated = String.valueOf(minutes1); }
+
+    if (hours1 >= 0 && hours1 < 10) {
+    hours_updated = "0" + hours1; }
+else {
+    hours_updated = String.valueOf(hours1); }
+
+    System.out.println(hours1 + ":" + minutes_updated);
+    //System.out.println("Hours: " + hours);
+    //System.out.println("Minutes: " + minutes);
+    //System.out.println("Minutes to add: " + minutesToAdd);
+    //System.out.println("New Hours: " + hours1);
+    //System.out.println("New Minutes: " + minutes1);
     }
 }
